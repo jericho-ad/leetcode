c721785b512c853f86ce4f975c8b739d93611151
Jericho <72528595+jericho-ad@users.noreply.github.com>
2022-01-09 17:28:44 -0500

    Create min-stack.py

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,45 @@
+class MinStack(object):
+
+    # Implemented using a list of tuples, where the last element contains the minimum value
+    # Allows for retrieving the minimum element in constant time while also only using one list
+    
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if not self.stack: 
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(self.stack[-1][1], val)))
+        
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
